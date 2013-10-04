81ad57a9563985e0fae390e9286ffb667c32e696
shigemk2 <i.am.shige@gmail.com>
2013-10-04 16:45:56 +0900

    Add popup

--- app/tab1_controller.rb
+++ app/tab1_controller.rb
@@ -2,7 +2,8 @@
 class Tab1Controller < UIViewController
   def init
     super
-    self.title = "ふあああああ"
+
+    self.title = "Tab 1"
     # self.tabBarItem.image = UIImage.imageNamed('FirstTab.png')
     self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemRecents, tag: 1)
     self
@@ -10,5 +11,22 @@ class Tab1Controller < UIViewController
 
   def viewDidLoad
     view.backgroundColor = UIColor.scrollViewTexturedBackgroundColor
+
+    setupNavigationBar
+  end
+
+  def setupNavigationBar
+    self.navigationController.navigationBar.barStyle = UIBarStyleBlackTranslucent
+    right_button_item = UIBarButtonItem.alloc.initWithTitle('Add', style:UIBarButtonItemStyleBordered, target: self, action: "click_add")
+    self.navigationItem.setRightBarButtonItem(right_button_item)
+  end
+
+  def click_add
+    @alert_box = UIAlertView.alloc.initWithTitle( "Add button popup",
+                                                 message:"You have pressed the Add button",
+                                                 delegate: nil,
+                                                 cancelButtonTitle: "ok",
+                                                 otherButtonTitles: nil)
+    @alert_box.show
   end
 end
